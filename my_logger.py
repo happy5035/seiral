b801b1db4721e498e66d0678924e735a5d10be0c
yejiawei <xjtuyjw@163.com>
2018-01-23 20:31:29 +0800

    配置logger文件

--- my_logger.py
+++ my_logger.py
@@ -4,14 +4,14 @@ from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger()
 logging.basicConfig(
-                    format='[%(asctime)s %(name)s %(levelname)s line:%(lineno)d] %(message)s',
+                    format='[%(asctime)s %(name)s-%(levelname)s-%(module)s-line:%(lineno)d] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.DEBUG)
 console = logging.StreamHandler()
 console.setLevel(logging.DEBUG)
 formatter = logging.Formatter('[%(asctime)s %(name)s-%(levelname)s-%(module)s-line:%(lineno)d] %(message)s')
 console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+# logging.getLogger('').addHandler(console)
 fileshandle = logging.handlers.TimedRotatingFileHandler('log/myLog', when='D', interval=1)
 fileshandle.suffix = "%Y%m%d_%H%M%S.log"
 fileshandle.setLevel(logging.DEBUG)
